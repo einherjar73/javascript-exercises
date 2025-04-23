82ba9195ce5a058f59c0207680582e78fb60a413
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-23 22:37:25 +0400

    finished

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  const newArr = books.map(book => {
+    return book.title;
+  })
+  return newArr;
 };
 
 // Do not edit below this line
