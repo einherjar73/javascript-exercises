cc7db810a21984a791b6a8d0783f5d35ac44cde0
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-23 15:53:44 +0400

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {  
+  const letters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  let chars = str.toLowerCase().split('');
+  chars = chars.filter(char => {
+    return letters.includes(char) ? true : false;    
+  });
+  let textReverse = chars.slice().reverse().toString();
+  let text = chars.toString();
+  
+  return text === textReverse;
 };
 
 // Do not edit below this line
