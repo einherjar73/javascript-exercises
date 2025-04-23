e3eec369e31fc0533c6b6df1dd684c0547fb24f3
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-23 22:11:25 +0400

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {  
+  let lastNum = 1;
+  let temp;
+  let previousNum = 1; 
+  if (n == 0){
+    lastNum = 0;
+  } else if (n == 1 || n == 2){
+    return lastNum;
+  } else if (n > 2) {
+    for(let i = 3; i <= n; i++){
+      temp = lastNum;
+      lastNum = previousNum + lastNum;
+      previousNum = temp;      
+    }
+  } else {
+    lastNum = "OOPS";
+  }  
+  return lastNum;
 };
 
 // Do not edit below this line
