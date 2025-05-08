fee515296802b1a8a0e649c6fb3bb3a336f054cc
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-05-08 13:35:20 +0400

    finished exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const thisYear = new Date().getFullYear();
+  let oldestPerson = 0;
+  const oldest = people.map((person, index) => {
 
+    if (!person.yearOfDeath) {
+      if (thisYear - person.yearOfBirth > oldestPerson) {
+        oldestPerson = thisYear - person.yearOfBirth;
+        idxOfOld = index;
+      }
+    } else {
+      if (person.yearOfDeath - person.yearOfBirth > oldestPerson) {
+        oldestPerson = person.yearOfDeath - person.yearOfBirth;
+        idxOfOld = index;
+      }
+    }    
+  });
+
+  return people[idxOfOld];
 };
 
 // Do not edit below this line
